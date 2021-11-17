89045b8a1c16ead0924d86e1cc5948f37b444e25
raviteja6337 <ravitejaboosa27@gmail.com>
2021-11-17 21:23:10 +0530

    Adding Room DB Repository Flow, removing the comments.

--- app/src/main/java/com/nooble/noobleaudio/data/network/ApiInterface.java
+++ app/src/main/java/com/nooble/noobleaudio/data/network/ApiInterface.java
@@ -5,15 +5,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Url;
 
-/**
- * * @author Ankit Jangid
- * * updated on : Sept.29,2021
- *
- * <p>
- * Summary:  API end points and function calls declarations
- * </p>
- */
-
 public interface ApiInterface {
 
     String GET_SHORT_DETAILS = "v3/b9f74279-038b-4590-9f96-7c720261294c/";
